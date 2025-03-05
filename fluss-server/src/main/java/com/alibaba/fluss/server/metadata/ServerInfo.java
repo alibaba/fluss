61cf1fc6587fc5ff62563aff9cf18076ef75907f
Hongshun Wang <loserwang1024@gmail.com>
2025-03-05 14:31:46 +0800

    Add serverInfo and ServerCluster as server metadata cache.

---
+++ fluss-server/src/main/java/com/alibaba/fluss/server/metadata/ServerInfo.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2024 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.fluss.server.metadata;
+
+import com.alibaba.fluss.cluster.Endpoint;
+import com.alibaba.fluss.cluster.ServerNode;
+import com.alibaba.fluss.cluster.ServerType;
+
+import javax.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * ServerInfo is used to save the endpoint metadata in controller and synchronize for each server.
+ */
+public class ServerInfo {
+    private final Integer id;
+    private final Map<String, Endpoint> endpointMap;
+
+    public ServerInfo(Integer id, List<Endpoint> endpoints) {
+        this.id = id;
+        this.endpointMap =
+                endpoints.stream()
+                        .collect(Collectors.toMap(Endpoint::getListenerName, endpoint -> endpoint));
+    }
+
+    public Integer id() {
+        return id;
+    }
+
+    public Endpoint endpoint(String listenerName) {
+        return endpointMap.get(listenerName);
+    }
+
+    public List<Endpoint> endpoints() {
+        return new ArrayList<>(endpointMap.values());
+    }
+
+    public @Nullable ServerNode toTableServerNode(String listenerName) {
+        if (endpoint(listenerName) == null) {
+            return null;
+        }
+        Endpoint endpoint = endpoint(listenerName);
+        return new ServerNode(id, endpoint.getHost(), endpoint.getPort(), ServerType.TABLET_SERVER);
+    }
+
+    public @Nullable ServerNode toCoordinatorServerNode(String listenerName) {
+        if (endpoint(listenerName) == null) {
+            return null;
+        }
+        Endpoint endpoint = endpoint(listenerName);
+        return new ServerNode(id, endpoint.getHost(), endpoint.getPort(), ServerType.COORDINATOR);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ServerInfo that = (ServerInfo) o;
+        return Objects.equals(id, that.id) && Objects.equals(endpointMap, that.endpointMap);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, endpointMap);
+    }
+}
